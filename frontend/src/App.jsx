37e1c6a050b0e880a6605487cb279a3c09dba583
Teresa Paatero <lotta.paatero@aalto.fi>
2024-02-23 19:12:59 +0200

    Backend setups made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Routes, Route, Link
+} from 'react-router-dom'
+
+
+const BookForm = () => {
+  return ('Hello world, I am at BookForm')
+}
+
+const Home = () => {
+  return ('Hello world, I am at Home')
+}
+
+const Stats = () => {
+  return ('Hello world, I am at Stats')
+}
+
+const BookList = () => {
+  return ('Hello world, I am at BookList')
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const padding = {
+    padding: 5
+  }
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Link style={padding} to="/">Home</Link>
+        <Link style={padding} to="/form">Add new</Link>
+        <Link style={padding} to="/booklist">Book list</Link>
+        <Link style={padding} to="/stats">Stats</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Routes>
+        <Route path="/form" element={<BookForm />} />
+        <Route path="/booklist" element={<BookList />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+
   )
 }
 
